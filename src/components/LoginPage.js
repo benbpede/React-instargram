737e54850d3a156e4fdd5369edf20f4d7961afc7
benbpede <benbpede@gmail.com>
2022-04-18 22:22:38 +0900

    로그인 페이지 제작중

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./LoginPage.scss";
 
 const LoginPage = () => {
   return (
@@ -6,28 +7,46 @@ const LoginPage = () => {
       <section>
         <main>
           <article>
-            <div>스마트폰 목업</div>
-            <div>
-              <h1>Instagram</h1>
-              <div>
-                <input
-                  type="text"
-                  placeholder="전화번호, 사용자 이름 또는 이메일"
-                />
-                <input type="text" placeholder="비밀번호" />
-                <button>로그인</button>
+            <div className="phone-mockup">스마트폰 목업</div>
+            <div className="login-and-signup">
+              <div className="login-box">
+                <h1>Instagram</h1>
+                <div className="login-input">
+                  <label className="id-input">
+                    <span>전화번호, 사용자 이름 또는 이메일</span>
+                    <input
+                      type="text"
+                      aria-label="전화번호, 사용자 이름 또는 이메일"
+                      required
+                      name="username"
+                      maxLength="75"
+                    />
+                  </label>
+                  <label className="pw-input">
+                    <span>비밀번호</span>
+                    <input
+                      type="text"
+                      aria-label="비밀번호"
+                      required
+                      name="password"
+                      autoCorrect="off"
+                    />
+                  </label>
+                  <button>로그인</button>
+                </div>
+                <p>비밀번호를 잊으셨나요?</p>
               </div>
-              <p>비밀번호를 잊으셨나요?</p>
-            </div>
-            <div>
-              <p>계정이 없으신가요?</p>
-              <button>가입하기</button>
-            </div>
-            <div>
-              <p>앱을 다운로드하세요.</p>
+
+              <div className="signup-box">
+                <p>계정이 없으신가요?</p>
+                <button>가입하기</button>
+              </div>
               <div>
-                <div>앱스토어 다운</div>
-                <div>구글 스토어 다운</div>
+                <p>앱을 다운로드하세요.</p>
+                <div>
+                  <div>앱스토어 다운</div>
+                  <div>구글 스토어 다운</div>
+                </div>
               </div>
             </div>
           </article>
